f4a655004af8d5b4dbdb825935a1438e467fb838
Güldeniz Özdemir <78044804+Deniz000@users.noreply.github.com>
2022-06-07 19:50:06 +0300

    added db connect

---
+++ ReCarProject/src/Helper/Config.java
@@ -0,0 +1,9 @@
+package Helper;
+
+public class Config {
+
+    public static final String PROJECT_TİTLE =  "Araç Kiralama Projesi";
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/recar_project";
+    public static final String DB_USERNAME = "postgres";
+    public static final String DB_PASSWORD = "12345";
+}
---
+++ ReCarProject/src/Helper/DbConnector.java
@@ -0,0 +1,22 @@
+package Helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnector {
+    Connection connection = null;
+
+    public Connection connectDb(){
+        try {
+            this.connection = DriverManager.getConnection(Config.DB_URL, Config.DB_USERNAME,Config.DB_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return this.connection;
+    }
+    public static Connection getInstance(){
+        DbConnector dbConnector = new DbConnector();
+        return dbConnector.connectDb();
+    }
+}
